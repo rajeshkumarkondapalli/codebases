35c32ca35f63e32bf94a4dd94ada4f166310ebbb
Rajesh Kondapalli <6193256+rajeshkumarkondapalli@users.noreply.github.com>
2025-02-17 10:07:29 -0500

    Update

---
+++ xmltest.java
@@ -0,0 +1,46 @@
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import java.io.ByteArrayInputStream;
+
+public class XPathUtils {
+    public static int countXPathOccurrences(String xml, String xpathExpression) {
+        try {
+            // Parse XML while ignoring namespaces
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            factory.setNamespaceAware(false); // Ignore namespaces
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document document = builder.parse(new ByteArrayInputStream(xml.getBytes()));
+
+            // Create XPath
+            XPathFactory xPathFactory = XPathFactory.newInstance();
+            XPath xpath = xPathFactory.newXPath();
+
+            // Compile and evaluate XPath expression
+            XPathExpression expr = xpath.compile(xpathExpression);
+            NodeList nodes = (NodeList) expr.evaluate(document, XPathConstants.NODESET);
+
+            // Return the count of matching nodes
+            return nodes.getLength();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return 0; // Return 0 in case of an error
+        }
+    }
+
+    public static void main(String[] args) {
+        String xml = "<root xmlns=\"http://example.com\">\n" +
+                     "    <item>Value</item>\n" +
+                     "    <item>Another Value</item>\n" +
+                     "</root>";
+
+        String xpathExpr = "//*[local-name()='item']"; // Ignore namespace
+        int count = countXPathOccurrences(xml, xpathExpr);
+        System.out.println("XPath occurrences: " + count);
+    }
+}
