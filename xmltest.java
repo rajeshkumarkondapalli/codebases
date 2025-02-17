3a21a3b2e912d06cad9033802c43e57601c6ae44
Rajesh Kondapalli <6193256+rajeshkumarkondapalli@users.noreply.github.com>
2025-02-17 10:21:52 -0500

    Update xmltest.java

--- xmltest.java
+++ xmltest.java
@@ -44,3 +44,44 @@ public class XPathUtils {
         System.out.println("XPath occurrences: " + count);
     }
 }
+
+
+
+
+
+
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import java.io.ByteArrayInputStream;
+
+public class Main {
+    public static void main(String[] args) {
+        String xml = "<root xmlns=\"http://example.com\">\n" +
+                     "    <item>Value</item>\n" +
+                     "    <item>Another Value</item>\n" +
+                     "</root>";
+
+        try {
+            // Parse XML
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            factory.setNamespaceAware(false); // Ignore namespaces
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document document = builder.parse(new ByteArrayInputStream(xml.getBytes()));
+
+            // Get root element
+            Element rootElement = document.getDocumentElement();
+
+            // Define XPath ignoring namespaces
+            String xpathExpr = "//*[local-name()='item']";
+
+            // Get count of occurrences
+            int count = XPathUtils.countXPathOccurrences(rootElement, xpathExpr);
+            System.out.println("XPath occurrences: " + count);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
