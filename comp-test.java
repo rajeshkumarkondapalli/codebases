7ce0d8969f7930eb309e368bc5a9a4e88b022b22
Rajesh Kondapalli <6193256+rajeshkumarkondapalli@users.noreply.github.com>
2025-03-10 15:51:59 -0400

    Update comp-test.java

--- comp-test.java
+++ comp-test.java
@@ -2,6 +2,54 @@ import java.util.*;
 
 class Test {
     String name;
+
+    Test(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        List<Test> list = Arrays.asList(
+            new Test("rajesh-8"),
+            new Test("rajesh-10"),
+            new Test("rajesh-9"),
+            new Test("rajesh-1"),
+            new Test("rajesh-100")
+        );
+
+        // Sort in numerical ascending order
+        list.sort(Comparator.comparing(t -> Integer.parseInt(t.getName().split("-")[1])));
+        System.out.println("Ascending: " + list);
+
+        // Sort in numerical descending order
+        list.sort(Comparator.comparing((Test t) -> Integer.parseInt(t.getName().split("-")[1])).reversed());
+        System.out.println("Descending: " + list);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+import java.util.*;
+
+class Test {
+    String name;
     int value;
 
     Test(String name, int value) {
